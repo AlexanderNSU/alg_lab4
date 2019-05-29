737b0710af7fb24b40543af44ff303caf2faa98c
AlexanderNSU <48973015+AlexanderNSU@users.noreply.github.com>
2019-05-29 17:31:01 +0700

    SelectSort

--- selection_sort.py
+++ selection_sort.py
@@ -2,4 +2,17 @@
 # -*- coding: utf-8 -*-
 
 def selection_sort(a):
-    pass
+    n=-1
+    for j in a:
+        n+=1
+        minim=j
+        m=n-1
+        change=n
+        for i in a[n:]:
+            m+=1
+            if i < minim:
+                minim=i
+                change=m
+        a[change]=a[n]
+        a[n]=minim
+    return a
