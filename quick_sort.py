3a84d1649a63dd7c1d6fe7dc1c43ecb464fe00cd
AlexanderNSU <48973015+AlexanderNSU@users.noreply.github.com>
2019-05-30 08:42:16 +0700

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,10 +1,25 @@
 def quicksort(a):
-    if a==[]:
-        return a
-
-    first = a[0]
-    lesser = quicksort([x for x in a[1:] if x < first])
-    greater = quicksort([x for x in a[1:] if x >= first])
-
-    return lesser + [first] + greater
-    
+    j=-1
+    i=-1
+    key=a[i]
+    for step in a:
+        j+=1
+        while (step >= key) and (j<len(a)+i-1):
+            i-=1
+            save=a[i]
+            a[i]=key
+            a[i+1]=step
+            a[j]=save
+            key=a[i]
+            step=a[j]
+        if (step >= key) :
+            a[j]=key
+            a[j+1]=step
+            break
+        elif j==len(a)+i-1:
+            break
+    if len(a[0:j+1])>=2:
+        a[0:j+1]=quicksort(a[0:j+1])
+    if len(a[j+1:])>=2:
+        a[j+1:]=quicksort(a[j+1:])
+    return a    
